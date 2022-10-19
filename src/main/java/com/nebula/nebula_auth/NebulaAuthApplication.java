b14cc04615fb9f216ebedad02617ba950faa8578
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-19 10:42:26 +0900

    feature: Initiate project

---
+++ src/main/java/com/nebula/nebula_auth/NebulaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nebula.nebula_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NebulaAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nebula/nebula_auth/NebulaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebula.nebula_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulaAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
