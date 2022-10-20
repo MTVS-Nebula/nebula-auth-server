27cbc45f6cabb821308d417c8bcb77548cbb3249
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-20 14:29:28 +0900

    refactor: 주석 추가 , validation 세분화

--- src/main/java/com/nebula/nebula_auth/helper/jwt/JwtUtil.java
+++ src/main/java/com/nebula/nebula_auth/helper/jwt/JwtUtil.java
@@ -1,8 +1,7 @@
 package com.nebula.nebula_auth.helper.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.nebula.nebula_auth.helper.jwt.dto.JwtDTO;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -19,38 +18,69 @@ public class JwtUtil {
     @Value("${jwt.access-token-validity-in-seconds}")
     private Integer accessExpireSecond;
 
+    /** token 에서 username 추출 */
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
+    /** token 에서 만료 시간 추출 */
     public Date extractExpiration(String token) {
         return extractClaim(token, Claims::getExpiration);
     }
+    /** token 에서 clameResolver(function) 를 통해 Claim 추출 -> 토큰의 subject 와 expiration 추출 시 사용 */
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
+    /** token 의 Claim body 추출 */
     private Claims extractAllClaims(String token) {
-        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        try {
+            //Jwts parser를 최근 버전의 빌드패턴인 parserBuilder 로 변경하여 사용
+            return Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e){
+            return e.getClaims();
+        }
     }
+    /** token Claim 의 expiration이 현재 시간보다 이전인지 확인  */
     private Boolean isTokenExpired(String token) {
         return extractExpiration(token).before(new Date());
     }
-    public String generateToken(String username) {
+    /** JwtDTO (id, username, roles) 를 통해 만든 토큰을 반환 */
+    public String generateToken(JwtDTO jwtDTO) {
 
         String token = null;
         Map<String, Object> claims = new HashMap<>();
-        token = createToken(claims, username);
+        claims.put("id", jwtDTO.getId());
+        claims.put("roles", jwtDTO.getRoles());
+        token = createToken(claims, jwtDTO.getUsername());
 
         return token;
     }
+    /** Map type의 claims, subject(username)으로 토큰 스트링을 생성 */
     private String createToken(Map<String, Object> claims, String subject) {
 
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * accessExpireSecond))
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
     }
+    /** 토큰의 유효성 검사 */
     public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        try {
+            // 토큰 파싱에서 오류가 발생하지 않는지 체크
+            Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token);
+            // 유저네임을 추출하여 해당 유저의 정보와 맞는지 && 토큰이 만료되었는지 체크
+            final String username = extractUsername(token);
+            return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            throw new RuntimeException("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            throw new RuntimeException("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("JWT 토큰이 잘못되었습니다.");
+        }
     }
 }
