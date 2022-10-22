834582db1e35c3f620e4b85f398c142b498f159c
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-22 23:34:37 +0900

    feature: Signup Data Transform Object 제작

---
+++ src/main/java/com/nebula/nebula_auth/app/dto/SignUpDTO.java
@@ -0,0 +1,49 @@
+package com.nebula.nebula_auth.app.dto;
+
+public class SignUpDTO {
+    private String username;
+    private String password;
+    private String email;
+
+    public SignUpDTO() {
+    }
+
+    public SignUpDTO(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "SignUpDTO{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
