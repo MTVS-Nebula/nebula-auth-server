927e5dd14524554ac19745ec7c7ab3c2933ae339
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-24 11:16:30 +0900

    fix(security): signup / login 모두에게 접근 가능 권한으로 변경

--- src/main/java/com/nebula/nebula_auth/config/security/SecurityConfiguration.java
+++ src/main/java/com/nebula/nebula_auth/config/security/SecurityConfiguration.java
@@ -13,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -66,7 +65,7 @@ public class SecurityConfiguration {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/login", "/signup").permitAll()
+                .antMatchers("/auth/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
