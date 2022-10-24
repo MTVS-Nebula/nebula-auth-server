10f67026be8a3e373b4ad0917a4a8e7f4f511236
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-10-24 13:36:03 +0900

    feature(login): 로그인 서비스 구현

--- src/main/java/com/nebula/nebula_auth/app/service/AuthService.java
+++ src/main/java/com/nebula/nebula_auth/app/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.nebula.nebula_auth.app.service;
 
+import com.nebula.nebula_auth.app.dto.LoginDTO;
 import com.nebula.nebula_auth.app.dto.SignUpDTO;
 
 public interface AuthService {
     public boolean signUp(SignUpDTO signUpDTO);
+    public String login(LoginDTO loginDTO);
 }
--- src/main/java/com/nebula/nebula_auth/app/service/impl/AuthServiceImpl.java
+++ src/main/java/com/nebula/nebula_auth/app/service/impl/AuthServiceImpl.java
@@ -4,32 +4,53 @@ import com.nebula.nebula_auth.app.dao.entity.Role;
 import com.nebula.nebula_auth.app.dao.entity.User;
 import com.nebula.nebula_auth.app.dao.entity.UserRole;
 import com.nebula.nebula_auth.app.dao.entity.embeddable.UserRoleId;
+import com.nebula.nebula_auth.app.dao.repository.LoginLogRepository;
 import com.nebula.nebula_auth.app.dao.repository.RoleRepository;
 import com.nebula.nebula_auth.app.dao.repository.UserRepository;
 import com.nebula.nebula_auth.app.dao.repository.UserRoleRepository;
+import com.nebula.nebula_auth.app.dto.LoginDTO;
 import com.nebula.nebula_auth.app.dto.SignUpDTO;
 import com.nebula.nebula_auth.app.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.nebula.nebula_auth.helper.jwt.JwtUtil;
+import com.nebula.nebula_auth.helper.jwt.dto.JwtDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthServiceImpl implements AuthService {
     private final UserRepository userRepository;
     private final UserRoleRepository userRoleRepository;
+    private final LoginLogRepository loginLogRepository;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public AuthServiceImpl(UserRepository userRepository, UserRoleRepository userRoleRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public AuthServiceImpl(UserRepository userRepository, UserRoleRepository userRoleRepository, LoginLogRepository loginLogRepository, JwtUtil jwtUtil, AuthenticationManager authenticationManager, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.userRoleRepository = userRoleRepository;
+        this.loginLogRepository = loginLogRepository;
+        this.jwtUtil = jwtUtil;
+        this.authenticationManager = authenticationManager;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    private final RoleRepository roleRepository;
-    private final PasswordEncoder passwordEncoder;
+//    public AuthServiceImpl(UserRepository userRepository, UserRoleRepository userRoleRepository, LoginLogRepository loginLogRepository, JwtUtil jwtUtil, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.userRoleRepository = userRoleRepository;
+//        this.loginLogRepository = loginLogRepository;
+//        this.jwtUtil = jwtUtil;
+//        this.roleRepository = roleRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
+
     @Override
     public boolean signUp(SignUpDTO signUpDTO) {
         try {
@@ -39,6 +60,23 @@ public class AuthServiceImpl implements AuthService {
         } catch (RuntimeException e){
             return false;
         }
+    }
+
+    @Override
+    public String login(LoginDTO loginDTO) {
+        String token = null;
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            loginDTO.getUsername(),
+                            loginDTO.getPassword()
+                    )
+            );
+            return generateToken(loginDTO);
+        } catch (Exception e){
+            throw new RuntimeException("login 에러");
+        }
+//        return null;
     }
 
     private User createUser(SignUpDTO signUpDTO){
@@ -55,5 +93,19 @@ public class AuthServiceImpl implements AuthService {
         Role role = roleRepository.findByRoleName(roleName);
         UserRole userRole = new UserRole(new UserRoleId(user, role));
         userRoleRepository.save(userRole);
+    }
+
+    private String generateToken(LoginDTO loginDTO){
+        try {
+            User user = userRepository.findByUsername(loginDTO.getUsername());
+            List<UserRole> userRoleList = userRoleRepository.findByIdUserUsername(loginDTO.getUsername());
+            List<String> roleList = userRoleList.stream()
+                    .map(uR ->uR.getId().getRole().getRoleName())
+                    .collect(Collectors.toList());
+            JwtDTO jwtDTO = new JwtDTO(user.getId(), user.getUsername(), roleList);
+            return jwtUtil.generateToken(jwtDTO);
+        } catch (RuntimeException e){
+            throw new RuntimeException("token 발행 에러");
+        }
     }
 }
