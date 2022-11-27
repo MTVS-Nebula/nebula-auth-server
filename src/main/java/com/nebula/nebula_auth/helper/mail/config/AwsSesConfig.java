6cb130448c62c0c3d711f04b2b0b22e67a8a67cc
DaeyeonKim97 <kdysy1130@khu.ac.kr>
2022-11-27 15:06:47 +0900

    feature(mail): aws ses 설정 파일 제작 #19

---
+++ src/main/java/com/nebula/nebula_auth/helper/mail/config/AwsSesConfig.java
@@ -0,0 +1,32 @@
+package com.nebula.nebula_auth.helper.mail.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AwsSesConfig {
+
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public AmazonSimpleEmailService amazonSimpleEmailService() {
+        final BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        final AWSStaticCredentialsProvider awsStaticCredentialsProvider = new AWSStaticCredentialsProvider(
+                basicAWSCredentials);
+
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+                .withCredentials(awsStaticCredentialsProvider)
+                .withRegion("ap-northeast-2")
+                .build();
+    }
+
+}
